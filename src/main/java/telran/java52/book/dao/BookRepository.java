79d86a7c823f15d8ea3d3668edacfbf993f63f54
Anton Viltovski <antonviltovski@gmail.com>
2024-06-26 10:42:24 +0300

    biderectional

--- src/main/java/telran/java52/book/dao/BookRepository.java
+++ src/main/java/telran/java52/book/dao/BookRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import telran.java52.book.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, String> {
-	Stream<Book> findByAuthorsName(String authorName);
 
-	Stream<Book> findByPublisherPublisherName(String publisherName);
-	
 	void deleteByAuthorsName(String name);
 }
